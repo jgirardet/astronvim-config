a9c47757917adfc6c0989a91fa50bf48be652448
Jimmy Girardet <ijkl@netc.fr>
2025-09-19 23:24:52 +0200

    remove unused git

--- lua/community.lua
+++ lua/community.lua
@@ -11,9 +11,6 @@ return {
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.nvim-tree-pairs" },
   { import = "astrocommunity.editing-support.wildfire-nvim" },
-  -- { import = "astrocommunity.git.fugit2-nvim" },
-  -- { import = "astrocommunity.git.diffview-nvim" },
-  -- { import = "astrocommunity.git.diffview-nvim" },
   -- packs
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
